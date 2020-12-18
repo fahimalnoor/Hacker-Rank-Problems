9b897678636f762552bdfc2ae28a38d6948e2716
Fahim Al-Noor <fahimalnoor1@gmail.com>
2020-12-19 04:41:16 +0600

    4. Practice

---
+++ 4_practice/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int a;
+int b = 9;
+
+for(a = 1; a < 20; a++) {
+
+if (a <= 10) {
+cout << a << " ";
+}
+
+else {
+cout << b-- <<" " ;
+
+}
+}
+}
