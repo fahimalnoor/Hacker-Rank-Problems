8c163e86299f82b2c142fda96bfc3f66dafd26ed
Fahim Al-Noor <fahimalnoor1@gmail.com>
2020-12-27 07:09:51 +0600

    30 days 4,5

---
+++ 30_days_code/5_multiplication/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for(int i = 1; i < 11; i++){
+
+        int mul = n*i;
+        cout << n;
+        cout<< " x " << i;
+        cout << " = " << mul << endl;
+    }
+    return 0;
+}
